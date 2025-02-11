dd68884b91091739386e372142a7308718ddf44a
Raph-01 <raphaelparadis@cmail.carleton.ca>
2025-02-11 17:10:18 -0500

    Update o3d_pose_estimation_6.py

--- o3d_pose_estimation_6.py
+++ o3d_pose_estimation_6.py
@@ -24,9 +24,9 @@ import copy
 
 ######################################### 1: User-defined Variables #######################################################
 Select_Mode = "Read" # "Read" "Live" "Record Live"
-platform = "Raph" # "Orin" "Raph" "Xavier"
-send_package_udp = False # [bool]
-visualizer_enabled = False # [bool]
+platform = "Orin" # "Orin" "Raph" "Xavier"
+send_package_udp = True # [bool]
+visualizer_enabled = True # [bool]
 
 ######################################### 3: Functions #######################################################
 def draw_result(source_pcd, target_pcd, transformation):
@@ -36,6 +36,37 @@ def draw_result(source_pcd, target_pcd, transformation):
     source_pcd_temp.paint_uniform_color([0, 0, 1]) # Blue
     target_pcd_temp.paint_uniform_color([1, 0, 0]) # Red
     o3d.visualization.draw_geometries([source_pcd_temp,target_pcd_temp, axis])
+
+def model_initialization(point_num, transform_model):
+    origin = np.array([0, 0, 0])
+    model = o3d.io.read_triangle_mesh(model_path)
+    model_pcd = model.sample_points_poisson_disk(number_of_points=point_num)
+    model_pcd = model_pcd.transform(transform_model)
+    model_pcd.scale(0.001, origin)
+
+    points = np.asarray(model_pcd.points)
+
+    half_width = 0.15
+    xmin = -half_width +0.005
+    xmax = half_width -0.005
+    ymin = -half_width +0.005
+    ymax = half_width -0.005
+    zmin = -1
+    zmax = 2*half_width +0.02
+
+    mask = np.logical_or(
+    np.logical_or(
+        (points[:, 0] < xmin) | (points[:, 0] > xmax),  # Check x-range
+        (points[:, 1] < ymin) | (points[:, 1] > ymax)   # Check y-range
+        ),
+        (points[:, 2] < zmin) | (points[:, 2] > zmax)  # Check z-range
+        )
+
+    filtered_points = points[mask]
+    filtered_model_pcd = o3d.geometry.PointCloud()
+    filtered_model_pcd.points = o3d.utility.Vector3dVector(filtered_points)
+
+    return filtered_model_pcd
 
 def receive_udp():
     IP_ADDRESS = '' # Set the IP to allow connections from any address
@@ -136,34 +167,19 @@ def registration_ICP_point2plane(model_pcd, target_pcd, distance_threshold_ICP, 
     return transform_ICP
 
 def registration_RANSAC(model_down, target_down, model_fpfh, target_fpfh, distance_threshold, confidence):
-    best_ransac = None
-    best_fitness = 0
     global visualizer_enabled
-    i = 0
-    loop_flag = True
-    while loop_flag:    
-        time_iter_start = time.time()
-        transform_RANSAC = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
-            model_down, target_down, model_fpfh, target_fpfh, True,
-            distance_threshold, 
-            o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
-            3, [o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
-            ], o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, confidence))
-        print(f"{i} Ransac {transform_RANSAC.fitness}")
-        time_iter_end = time.time()
-        print(f"Ransac duration = {time_iter_end-time_iter_start}s")
-        if transform_RANSAC.fitness > best_fitness:
-            best_fitness = transform_RANSAC.fitness
-            best_ransac = transform_RANSAC
-            draw_result(model_down, target_down, best_ransac.transformation)
-        if best_fitness > 0.75 or i > 4:
-            loop_flag = False
-        i+=1
-    if visualizer_enabled:
-        draw_result(model_down, target_down, best_ransac.transformation)
-    exit(0)
-    return best_ransac
+
+    transform_RANSAC = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        model_down, target_down, model_fpfh, target_fpfh, True,
+        distance_threshold, 
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+        o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, confidence))
+ 
+    # if visualizer_enabled:
+        # draw_result(model_down, target_down, transform_RANSAC.transformation)
+    return transform_RANSAC
 
 def estimate_pose(transformation):
     # Rz(gamma) Ry(beta) Rx(alpha) [degrees]
@@ -188,6 +204,14 @@ def estimate_pose(transformation):
     # y = round(transformation[1, 3]*1000,2)
     # z = round(transformation[2, 3]*1000,2)
     return [x, y, z, alpha, beta, gamma]
+
+def pcd_downsample_fpfh(pcd, voxel_size, radius_normal, radius_feature):
+    model_down = pcd.voxel_down_sample(voxel_size) # Downsample and estimate normals for point cloud
+    model_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+    # Compute FPFH features
+    model_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        model_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return model_down, model_fpfh
 
 def data_reduction_pcd(pcd, **kwargs):
     pcd_reduced = o3d.geometry.PointCloud()
@@ -226,13 +250,14 @@ def data_reduction_pcd(pcd, **kwargs):
 
     # *** For Y-axis, signs are resversed for some reason. Added top crop (will need to be updated for MECH mechanism. Too remove myself, is not needed if no manipulator)
     # Remove points that are too low (camera Y-axis). Is the target reflection
-    indicesZlow = np.where(verts[:,2] > 0.15)[0] # find where Z < 0.15 m (15cm)
-    verts = np.delete(verts, indicesZlow, axis=0)
+    indicesZhigh = np.where(verts[:,2] > 0.15)[0] # find where Z < 0.15 m (15cm)
+    verts = np.delete(verts, indicesZhigh, axis=0)
 
     # *** TO REMOVE
     # Remove points that are too high (camera Y-axis).
-    indicesZhigh = np.where(verts[:,2] < -0.5)[0] # find where Z > -0.15 m (15cm)
-    verts = np.delete(verts, indicesZhigh, axis=0)  
+    indicesZlow = np.where(verts[:,2] < -0.115)[0] # find where Z > -0.15 m (15cm)
+    verts = np.delete(verts, indicesZlow, axis=0)  
+    
     pcd_reduced.points = o3d.utility.Vector3dVector(verts)
     return pcd_reduced
 
@@ -251,23 +276,19 @@ transform_camera = np.dot(RotY90, RotZNeg90)
 # transform_camera[0,3] = 0.15
 # transform_camera[1,3] = 0.087
 transform_model = np.dot(RotZ90,RotX90)
+origin = np.array([0, 0, 0])
+axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=origin)
 
 # RANSAC and ICP parameters
 # (same for icp except ICP threshold 
-voxel_size_RANSAC_coarse = 0.05  # (m) Change depending on point cloud size (0.05)
-radius_normal_RANSAC_coarse = voxel_size_RANSAC_coarse * 4
-radius_feature_RANSAC_coarse = voxel_size_RANSAC_coarse * 7 # (10)
-distance_threshold_RANSAC_coarse = voxel_size_RANSAC_coarse * 1 # 1.5 (3)
-
-voxel_size_RANSAC_fine = 0.01
-radius_normal_RANSAC_fine = voxel_size_RANSAC_fine * 2
-radius_feature_RANSAC_fine = voxel_size_RANSAC_fine * 5
-distance_threshold_RANSAC_fine = voxel_size_RANSAC_fine * 2 # 1.5
-
-voxel_size_ICP = 0.01 # (0.05)
-distance_threshold_ICP = voxel_size_ICP * 0.4 # 0.4 (10)
-
+voxel_size_RANSAC = 0.05  # (m) Change depending on point cloud size (0.05)
+radius_normal_RANSAC = voxel_size_RANSAC * 4
+radius_feature_RANSAC = voxel_size_RANSAC * 7 # (10)
+distance_threshold_RANSAC = voxel_size_RANSAC * 1 # 1.5 (3)
 ransac_confidence = 0.9999
+
+voxel_size_ICP = 0.05 # (0.05)
+distance_threshold_ICP = voxel_size_ICP * 1 # 0.4 (10)
 
 # Set script states
 read_recording, record_enabled = (True, False) if Select_Mode == "Read" else (False, True) if Select_Mode == "Record Live" else (False, False)
@@ -299,55 +320,9 @@ elif platform == "Xavier":
 # Model Initialization
 try:
     print("Initializing Model: Start")
-    origin = np.array([0, 0, 0])
-    model = o3d.io.read_triangle_mesh(model_path)
-    model_pcd = model.sample_points_poisson_disk(number_of_points=1000)
-    # model_pcd = model_pcd.transform(transform_model)
-    model_pcd = model_pcd.transform(RotZ90)
-    model_pcd.scale(0.001, origin)
-
-    points = np.asarray(model_pcd.points)
-
-    half_width = 0.15
-    xmin = -half_width +0.005
-    xmax = half_width -0.005
-    ymin = -half_width +0.005
-    ymax = half_width -0.005
-    zmin = -1
-    zmax = 2*half_width +0.02
-
-    mask = np.logical_or(
-    np.logical_or(
-        (points[:, 0] < xmin) | (points[:, 0] > xmax),  # Check x-range
-        (points[:, 1] < ymin) | (points[:, 1] > ymax)   # Check y-range
-        ),
-        (points[:, 2] < zmin) | (points[:, 2] > zmax)  # Check z-range
-        )
-    
-    # Apply the mask to filter out points inside the box
-    filtered_points = points[mask]
-
-    # If you want to create a new PointCloud object with the filtered points
-    filtered_model_pcd = o3d.geometry.PointCloud()
-    filtered_model_pcd.points = o3d.utility.Vector3dVector(filtered_points)
-    # verts = np.delete(model_pcd, mask, axis=0)
-    
-    model_down = filtered_model_pcd.voxel_down_sample(voxel_size_ICP) # Downsample and estimate normals for point cloud
-    model_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal_RANSAC_fine, max_nn=30))
-    # Compute FPFH features
-    model_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
-        model_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature_RANSAC_fine, max_nn=100))
-    
+    model_pcd = model_initialization(1000, RotZ90)
+    model_down, model_fpfh = pcd_downsample_fpfh(model_pcd, voxel_size_RANSAC, radius_normal_RANSAC, radius_feature_RANSAC) # Downsample and estimate normals for point cloud
     model_down.paint_uniform_color([1, 0, 0]) # Red
-    
-    # Create the coordinate axis (standard Open3D axes with 1m length by default)
-    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=origin)  # Size=0.3m (30cm)
-    # X-axis -> Red (points along the X direction)
-    # Y-axis -> Green (points along the Y direction)
-    # Z-axis -> Blue (points along the Z direction)
-
-    # Visualize the filtered point cloud along with the coordinate axes
-    # o3d.visualization.draw_geometries([filtered_model_pcd, axis])
     print("Initializing Model: End")
 
 except KeyboardInterrupt:
@@ -357,6 +332,9 @@ except Exception as e:
     print(e)
     print("Could not find Target Model.")
     exit(0)
+
+# if visualizer_enabled:
+#    o3d.visualization.draw_geometries([model_down, axis])
 
 # Configure depth stream
 if not read_recording:    
@@ -378,6 +356,10 @@ if not read_recording:
     profile = pipeline.start(config)
 
 # Script Variables - General
+ground_truth_x = 9999
+ground_truth_y = 9999
+ground_truth_theta = 9999
+
 target_pcd = o3d.geometry.PointCloud()
 recording_header = ["Depth Verts", "Time Stamp"]
 recording = [recording_header]
@@ -429,47 +411,33 @@ try:
 
         verts = frame_data[0]
         target_pcd.points = o3d.utility.Vector3dVector(verts)
-        target_pcd.transform(transform_camera) 
-        
-        frame_timestamp = frame_data[1] # If recorded from old script index = 3, else index = 1
+        target_pcd.transform(transform_camera)
+        target_pcd = data_reduction_pcd(target_pcd)
+        target_down, target_fpfh = pcd_downsample_fpfh(target_pcd, voxel_size_RANSAC, radius_normal_RANSAC, radius_feature_RANSAC)
+        # if visualizer_enabled:
+        #    o3d.visualization.draw_geometries([target_down, axis])
 
-        # Data and noise reduction.
-        if transform is None: # Coarse Reduction
-            target_pcd = data_reduction_pcd(target_pcd)        
-        else: # Refined Reduction
-            target_pcd = data_reduction_pcd(target_pcd, target_transform=transform.transformation)
-        # target_pcd = noise_reduction_pc(target_pcd)
+        frame_timestamp = frame_data[3] # If recorded from old script index = 3, else index = 1
         
         # RANSAC
         if transform is None:
             ransac_valid = False
             while not(ransac_valid):
-                target_down = target_pcd.voxel_down_sample(voxel_size_RANSAC_coarse) # Downsample and estimate normals for point cloud
                 try: 
-                    target_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal_RANSAC_coarse, max_nn=30))  
-                    target_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
-                        target_down, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature_RANSAC_coarse, max_nn=100))
-                    transform = registration_RANSAC(model_down, target_down, model_fpfh, target_fpfh, distance_threshold_RANSAC_coarse, ransac_confidence)
+                    transform = registration_RANSAC(model_down, target_down, model_fpfh, target_fpfh, distance_threshold_RANSAC, ransac_confidence)
                     RANSAC_transform_fitness = transform.fitness
                     RANSAC_transform_rmse = transform.inlier_rmse
-                    print(f"{time.strftime('%H:%M:%S')} RANSAC Success")
-                
-                    # Logic to validate ICP output to bee added in the ICP criteria. Check what is the error output of registration_icp
-                    # transform_relative = np.dot(transform_camera,transform.transformation) # Transform from camera frame to inertial standard
+                    print(f"{time.strftime('%H:%M:%S')} RANSAC Success. RANSAC Fitness = {RANSAC_transform_fitness}")
+                                    
                     RANSAC_pose = estimate_pose(transform.transformation) # X, Y, Z, Rx, Ry, Rz (millimeters and degrees, relative Chaser-Target)
                     RANSAC_timestamp = time.strftime('%H:%M:%S')
                     print(RANSAC_pose)
-                    if visualizer_enabled:
-                        draw_result(model_down, target_down, transform.transformation)
-                    target_pcd = data_reduction_pcd(target_pcd)
-                    """UNCOMMENT BELOW TO TEST RANDSAC ONLY""" 
-                    # transform = None
-                    # continue
-                
-                    # Check if RANSAC output is valid
+                                        # Check if RANSAC output is valid
                     valid_Rx = RANSAC_pose[3]%180 < 5 or RANSAC_pose[3]%180 > 175
                     valid_Ry = RANSAC_pose[3]%180 < 5 or RANSAC_pose[3]%180 > 175
-                    ransac_valid = valid_Rx and valid_Ry
+                    valid_fitness = RANSAC_transform_fitness > 0.5
+                    ransac_valid = valid_Rx and valid_Ry and valid_fitness
+                    print(f"Ransac valid: {ransac_valid}")
 
                 except KeyboardInterrupt:
                     print("\nCtrl+C pressed, exiting loop...")
@@ -480,10 +448,13 @@ try:
                     print(f"{time.strftime('%H:%M:%S')} RANSAC Fail")
                     continue
 
+        if visualizer_enabled:
+            draw_result(model_down, target_down, transform.transformation)
+
         # ICP REFINEMENT
         try:
-            target_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-            transform = registration_ICP_point2plane(model_down, target_pcd, distance_threshold_ICP, transform)
+            target_down.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+            transform = registration_ICP_point2plane(model_down, target_down, distance_threshold_ICP, transform)
             ICP_transform_fitness = transform.fitness
             ICP_transform_rmse = transform.inlier_rmse
             print(f"{time.strftime('%H:%M:%S')} ICP Success")
@@ -503,12 +474,15 @@ try:
         transform_result_y = ICP_pose[1]+0.087
         transform_result_theta = ICP_pose[5]
 
+        
         try:
             perc_error_x = (transform_result_x-ground_truth_x)*100/ground_truth_x
             perc_error_y =(transform_result_y-ground_truth_y)*100/ground_truth_y
             perc_error_theta =(transform_result_theta-ground_truth_theta)*100/ground_truth_theta
-        finally:
             print(f"% Error X = {perc_error_x}, Error Y = {perc_error_y}, Error Theta = {perc_error_theta}.")
+
+        except Exception as e:
+            print(e)
         
         if send_package_udp:
             # + values below to account for camera offset
@@ -528,12 +502,15 @@ try:
                                 ground_truth_x, ground_truth_y, ground_truth_theta])
 
         if visualizer_enabled:
-            draw_result(model_down, target_pcd, transform.transformation)
+            draw_result(model_down, target_down, transform.transformation)
         
         # if not(reset_counter%20):
         #    print("reset transform")
         #    transform = None
         reset_counter +=1
+
+except Exception as e:
+    print(e)
 
 finally:
     np.savetxt(pose_estimation_path, transform_array, fmt="%s", delimiter=',')
